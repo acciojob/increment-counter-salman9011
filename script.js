f984102962249e38ca58af3c887522afa24df232
salman <70580281+salman9011@users.noreply.github.com>
2024-01-08 13:37:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
     let button = document.getElementById("incrementBtn");
-    
-
 button.addEventListener("click", function () {
-	let count = parseInt(counter.innerText);
 	let counter = document.getElementById("counter");
+	let count = parseInt(counter.innerText);
+	
       // Display the un-incremented value in an alert
-      alert(" un-incremented value "+ count);
+      alert(count);
 
       // Increment the counter value and update the display
       counter.innerText = ++count;
