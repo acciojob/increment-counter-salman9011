810e854f6ce3e58209c92994fb5b63f1b11b81dc
salman <70580281+salman9011@users.noreply.github.com>
2024-01-08 12:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 let counter=document.getElementById("counter");
 let btn=document.getElementById("incremmentBtn");
-function click(){
+let count=0;
+function increment(){
+	alert(count);
+	counter.innerText= count++;
 	
 }
